857665e7dbac04ccb355d6a047648222c88aba69
FernandoSan3 <fernando_0257@hotmail.com>
2021-01-07 12:56:46 -0500

    Iniciar la Practica

---
+++ Practica4/src/ec/edu/ups/clases/Logica.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.ups.clases;
+
+/**
+ *
+ * @author fernandosanchez
+ */
+public class Logica {
+    
+    public String metodoIntegrante1(){
+        return "integrante1";
+    }
+    public String metodoIntegrante2(){
+        return "integrante2";
+    }
+
+}
